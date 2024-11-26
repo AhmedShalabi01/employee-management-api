d0a787c5894e563fb8067228b6fc17de064251f2
AhmedShalabi01 <shtk3eb01@gmail.com>
2024-11-26 22:41:17 +0300

    Add the impl for finding, adding and deleting employee for the controller

--- src/main/java/com/springcourse/employeecrud/controller/EmployeeRestController.java
+++ src/main/java/com/springcourse/employeecrud/controller/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.springcourse.employeecrud.dao.EmployeeDAO;
 import com.springcourse.employeecrud.entity.Employee;
 import com.springcourse.employeecrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +23,22 @@ public class EmployeeRestController {
     public List<Employee> findAll(){
         return employeeService.findAll();
     }
-
+    @GetMapping("/employees/{id}")
+    public Employee findById(@PathVariable int id){
+        return employeeService.findById(id);
+    }
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+        return employeeService.save(employee);
+    }
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployee(@PathVariable int id) {
+        employeeService.deleteById(id);
+    }
 
 }
